ab929b8333432210969b5a98506c7a1883a8c424
Chirag <chirag@Chirags-MacBook-Air.local>
2025-08-06 23:56:59 +0530

    fixing errors 4

--- .history/src/app/interview/[id]/perform/actions_20250806093516.ts
+++ .history/src/app/interview/[id]/perform/actions_20250806093516.ts
@@ -4,12 +4,7 @@ import client from "@/lib/db";
 import axios from "axios";
 import { ObjectId } from "mongodb";
 
-// const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000'
-  : 'https://ai-interview-iota-ten.vercel.app';
-
-
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 export const getInterviewDetails = async (id:string)=>{
     if(!id){
